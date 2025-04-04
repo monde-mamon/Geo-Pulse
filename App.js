9e909131b099558236e4d203e733efa725abc4c3
mond-dlr17 <delrosarioraymond17@gmail.com>
2025-04-04 16:59:53 +0800

    feat: add app.js to make it project snackable

---
+++ App.js
@@ -0,0 +1,20 @@
+import { Text, SafeAreaView, StyleSheet } from 'react-native';
+
+export default function App() {
+  return <SafeAreaView style={styles.container}>Hello World</SafeAreaView>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
